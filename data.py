b2899027974458430e20b63a8671f59085159635
akbasal <alperenakbas42@hotmail.com>
2025-07-19 20:25:41 +0300

    recursive construct_decision_tree function was implemented successfuly

--- data.py
+++ data.py
@@ -13,7 +13,7 @@ def featurize_data(path):
 def get_x_y(path):
     df = pd.read_csv(path)
     y = df['class'].to_numpy()
-    x = df.drop(columns=['class'])
+    x = df.drop(columns=['class']).to_numpy()
 
 
     folds = np.array_split(np.arange(8124), 10)
@@ -22,8 +22,8 @@ def get_x_y(path):
     for i in range(10):
         val_idx = folds[i]
         train_idx = np.concatenate(folds[:i] + folds[i+1:])
-        x_train, y_train = x.iloc[train_idx], y[train_idx]
-        x_val, y_val = x.iloc[val_idx], y[val_idx]
+        x_train, y_train = x[train_idx], y[train_idx]
+        x_val, y_val = x[val_idx], y[val_idx]
         splits.append((x_train, y_train, x_val, y_val))
     
     return splits
--- decision_tree.py
+++ decision_tree.py
@@ -28,29 +28,64 @@ def calculate_information_gain(x_train,y_train, index):
     right_count = 0
     left_count = 0
     for i in range(len(x_train)):
-        if x_train.iloc[i,index]:
-            right_x_train.append(x_train.iloc[i])
+        if x_train[i][index]:
+            right_x_train.append(x_train[i])
             right_y_train.append(y_train[i])
             right_count+=1
         else:
-            left_x_train.append(x_train.iloc[i])
+            left_x_train.append(x_train[i])
             left_y_train.append(y_train[i])
             left_count+=1
     right_weight = right_count/(right_count+left_count)
     left_weight = left_count/(right_count+left_count)
     if left_count == 0 or right_count == 0:
-        return 0,index
-    information_gain = entropy_function(np.array(y_train)) - (right_weight*entropy_function(np.array(right_y_train)))+(left_weight*entropy_function(np.array(left_y_train)))
+        return right_x_train , right_y_train , left_x_train , left_y_train , 0
+    information_gain = entropy_function(np.array(y_train)) - ((right_weight*entropy_function(np.array(right_y_train)))+(left_weight*entropy_function(np.array(left_y_train))))
+    return (
+    np.array(right_x_train),
+    np.array(right_y_train),
+    np.array(left_x_train),
+    np.array(left_y_train),
+    information_gain
+)
+
+
+def calculate_max_information_gain(x_train,y_train):
+    gain = 0
+    max_gain = 0
+    max_gain_index = 0
+    right_x_train = np.array([])
+    right_y_train = np.array([])
+    left_x_train = np.array([])
+    left_y_train = np.array([])
+
+    for i in range(x_train.shape[1]):
+        xright_x_train,xright_y_train,xleft_x_train,xleft_y_train,gain = calculate_information_gain(x_train, y_train, i)
+        if (gain > max_gain):
+            right_x_train,right_y_train,left_x_train,left_y_train =  xright_x_train,xright_y_train,xleft_x_train,xleft_y_train
+            max_gain =gain
+            max_gain_index=i
+    return right_x_train,right_y_train,left_x_train,left_y_train,max_gain, max_gain_index
     
-    return information_gain,index
 
 
+def construct_decision_tree(x_train,y_train,decision_tree,index=0, iterations=5):
+    # tree will be an array
+    # where root is i, left node is 2i+1 and right node is 2i+2
+    right_x_train,right_y_train,left_x_train,left_y_train,max_gain, max_gain_index = calculate_max_information_gain(x_train, y_train)
+    if(max_gain<0.001): return
+    decision_tree[index] = max_gain_index
+    if (iterations > 0):
+        construct_decision_tree(left_x_train, left_y_train,decision_tree, 2*index+1, iterations-1)
+        construct_decision_tree(right_x_train, right_y_train, decision_tree, 2*index+2, iterations-1)
 
-
-
+    
+ 
+    return 0
 
 x_train,y_train,x_val,y_val = splits[0]
-print(x_train.shape)
-print(y_train.shape)
 
-print(calculate_information_gain(x_train,y_train,2))
+decision_tree = np.zeros(33)
+construct_decision_tree(x_train,y_train,decision_tree)
+print(decision_tree)
+
